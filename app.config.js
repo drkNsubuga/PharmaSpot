00ecf67f0afacfa9efc5f81d6ff37fc00345b4bf
Nsubuga Derrick <rite2drk@gmail.com>
2023-12-31 16:30:08 +0300

    Changed update server - Fixed deprecation notice for dates with momentjs

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 /*app.config.js*/
 let appConfig = {
-	UPDATE_SERVER: "https://update-center.onrender.com",
+	UPDATE_SERVER: "https://download.pharmaspot.patternsdigital.com",
 	// about 
 	COPYRIGHT_YEAR: "2022",
 }
